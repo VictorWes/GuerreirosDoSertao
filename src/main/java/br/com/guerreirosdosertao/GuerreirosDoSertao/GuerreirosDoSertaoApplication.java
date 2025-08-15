5ef70bc91d3b3ef4130e6c2c6ece0a30c832ad31
Victor Wesley <victor.infowb@gmail.com>
2025-08-14 21:02:57 -0300

    chore: Estrutura inicial para inicio do projeto

---
+++ src/main/java/br/com/guerreirosdosertao/GuerreirosDoSertao/GuerreirosDoSertaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guerreirosdosertao.GuerreirosDoSertao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuerreirosDoSertaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuerreirosDoSertaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guerreirosdosertao/GuerreirosDoSertao/GuerreirosDoSertaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guerreirosdosertao.GuerreirosDoSertao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuerreirosDoSertaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
